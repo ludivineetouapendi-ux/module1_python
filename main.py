45a51145bc81bf49fb2bc0b94aac6fd66f89ace5
Ludivine_EA <letou@eugeniaschool.com>
2025-10-02 13:13:09 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+# Exercice 1
+# age = 77
+# height = 1.75
+# name = 'ludivine'
+# is_student = True
+# print(f"Age: {age}, Type: {type(age)}")
+# print(f"height: {height}, Type: {type(height)}")
+# print(f"name: {name}, Type: {type(name)}")
+# print(f"is_student: {is_student}, Type: {type(is_student)}")
+
+# Exercice 2
+# a = 78
+# b = 96
+
+# addition = a + b
+# subtraction = a - b
+# multiplication = a * b
+# division = a / b
+
+# print(f"Addition: {a + b}, Type: {type(addition)}")
+# print(f"Subtraction: {a - b}, Type: {type(subtraction)}")
+# print(f"Multiplication: {a * b}, Type: {type(multiplication)}")
+# print(f"Division: {a / b}, Type: {type(division)}")
+# remainder = a % b
+# power = a ** b
+# print(f"remainder: {a / b}")
+# print(f"power: {a ** b}")
+
+#Exercice 3
+
+# first_name = "ludivine "
+# last_name = "Etou Apendi"
+# full_name = first_name + "" + last_name
+# print("Full name in uppercase:", full_name.upper())
+# print("Length of full name:", len(full_name))
+# sentence = "Python est un langage puissant"
+# words_list = sentence.split()
+# print("Original sentence:", sentence)
+# print("Words list:", words_list)
+
+
+# Exercice 4
+# Convertir une chaîne contenant un nombre en entier
+str_number = "42"
+int_number = int(str_number)
+
+# Convertir un float en entier
+float_number = 3.99
+int_from_float = int(float_number)  # ça tronque la partie décimale (pas d'arrondi)
+
+# Convertir un entier en float
+int_value = 7
+float_from_int = float(int_value)
+
+# Convertir un nombre (int ou float) en string
+num = 123
+str_from_num = str(num)
+
+# Affichage des résultats et de leurs types
+print(f"Original string: {str_number}, Converted: {int_number}, Type: {type(int_number)}")
+print(f"Original float: {float_number}, Converted: {int_from_float}, Type: {type(int_from_float)}")
+print(f"Original int: {int_value}, Converted: {float_from_int}, Type: {type(float_from_int)}")
+
+
+#Exercice 4
+str_number = "42"
+int_number = int(str_number)
+
+float_number = 3,99
+int_from_float = int(float_number)
+
+int_value = 7
+float_from_int =float(int_value)
+
+num = 123
+str_from_int = str(num)
+
+print(f"Original string: {str_number}, Converted: {int_number}, Type: {type(int_number)}")
+print(f"Original float: {float_number}, Converted: {int_from_float}, Type: {type(int_from_float)}")
+print(f"Original int: {int_value}, Converted: {float_from_int}, Type: {type(float_from_int)}")
+print(f"Original number: {num}, Converted: '{str_from_num}', Type: {type(str_from_num)}")
+
